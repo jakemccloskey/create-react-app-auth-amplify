4a3dce9e5617bb9a4ecd01fbaefd797bb8b65257
Jake McCloskey <jakemccloskey@gmail.com>
2020-07-26 01:27:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 2
+            Learn React 3
           </a>
         </header>
       </div>
